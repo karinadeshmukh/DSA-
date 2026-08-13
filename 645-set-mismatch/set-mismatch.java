8ab660e25f69055713525fceafe5d6772626acd3
karinadeshmukh <karinavd294@gmail.com>
2026-08-13 23:39:46 +0530

    Time: 2 ms (94.55%) | Memory: 47.8 MB (34.92%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int duplicate = 0;
+        int missing = 0 ;
+        int[] count = new int[nums.length + 1];
+
+        for(int i= 0; i<nums.length;i++){
+            count[nums[i]]++;
+        }
+
+        for(int i = 1;i<=nums.length;i++){
+            if(count[i] == 2){
+                duplicate = i;
+            }
+            else if(count[i] == 0){
+                missing = i;
+            }
+            
+        }
+        return new int[]{duplicate,missing};
+    }
+}
